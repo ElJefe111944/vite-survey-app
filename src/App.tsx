8114db5bd9fa9729500fab17cccb0527e36e648e
James Farr <jamesfarr1@sky.com>
2025-05-17 12:59:35 +0100

    Home page created and route added to app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
