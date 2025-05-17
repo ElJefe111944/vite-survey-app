c0d51b173efc9c3db0efd83a86c2c4c2b9aec945
James Farr <jamesfarr1@sky.com>
2025-05-17 14:49:39 +0100

    feat(survey): load survey function added to survey page

--- src/pages/Survey.tsx
+++ src/pages/Survey.tsx
@@ -1,11 +1,42 @@
-
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchSurvey } from "../lib/api";
+import type { Survey } from "../lib/interface";
 
 const Survey = () => {
+    const { id } = useParams();
 
+    const [survey, setSurvey] = useState<Survey | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+
+        const loadSurvey = async () => {
+
+            if(!id) return;
+
+            try {
+                const data = await fetchSurvey(id);
+                setSurvey(data);
+            } catch (error) {
+                setError("Failed to load surveys");
+                console.error("Failed to load surveys: ", error);
+            } finally {
+                setIsLoading(false);
+            };
+        };
+
+        loadSurvey();
+    }, [id]);
+
+    if (isLoading) return <p>Loading survey...</p>
+    if (error || !survey) return <p>{error || "Survey not found"}</p>
 
     return (
         <div>
-            Survey
+            <p>{survey.title}</p>
+            <p>{survey.description}</p>
         </div>
     )
 };
