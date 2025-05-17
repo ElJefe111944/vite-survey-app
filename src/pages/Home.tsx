eaf6ded833677e4aadbfbd94a6b732b0a29bca6e
James Farr <jamesfarr1@sky.com>
2025-05-17 13:09:31 +0100

    feat(loadsurveys): load surveys function added to home page to update survey state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import { fetchSurveys } from "../lib/api";
 
 const Home = () => {
+
+    const [surveys, setSurveys] = useState([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const loadSurveys = async () => {
+            console.log("Loading surveys")
+            try {
+                const data = await fetchSurveys();
+                setSurveys(data);
+            } catch (error) {
+                setError("Failed to load surveys");
+                console.error("Failed to load surveys: ", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        loadSurveys();
+    }, []);
 
     return (
         <h1 className="text-center">HomePage</h1>
