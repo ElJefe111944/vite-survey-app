5dc540ba441576ab4efe062be342ca73e846f731
James Farr <jamesfarr1@sky.com>
2025-05-17 16:13:22 +0100

    feat(api): submit survey api call added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import type { Survey, SaveResponseAck, SurveyResponsesOut } from "./interface";
 
 const API_BASE = import.meta.env.VITE_API_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -10,12 +11,20 @@ const api = axios.create({
     }
 });
 
-export async function fetchSurveys(){
+export async function fetchSurveys(): Promise<Survey[]> {
     const res = await api.get("/surveys");
     return res.data;
 };
 
-export async function fetchSurvey(id: string){
+export async function fetchSurvey(id: string): Promise<Survey> {
     const res = await api.get(`/surveys/${id}`);
+    return res.data;
+};
+
+export async function submitSurvey(
+    id: string,
+    payload: SurveyResponsesOut
+): Promise<SaveResponseAck> {
+    const res = await api.post(`/surveys/${id}/response`, payload);
     return res.data;
 };
