9aebf577b575292f41e26cb2cb724172545ec9fc
James Farr <jamesfarr1@sky.com>
2025-05-17 12:40:43 +0100

    configure axios instance with base URL and static API key

---
+++ src/lib/api.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const api = axios.create({
+    baseURL: API_BASE,
+    headers: {
+        Authorization: API_KEY,
+    }
+});
