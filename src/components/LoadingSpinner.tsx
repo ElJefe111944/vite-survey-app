c05528b29918a2a8e6ca60a8bd7aeb69fed91e2a
James Farr <jamesfarr1@sky.com>
2025-05-18 16:57:46 +0100

    loading spinner styling added

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -3,8 +3,8 @@ import { GridLoader } from "react-spinners";
 const LoadingSpinner = () => {
 
     return (
-            <div className="w-full h-full absolute flex justify-center items-center">
-                <GridLoader color="black" />
+            <div className="w-full h-full absolute flex justify-center items-center bg-white">
+                <GridLoader color="oklch(69.6% 0.17 162.48)" />
             </div>
     )
 };
