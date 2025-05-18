4961d557346a9daae1a501a5f02267f163fd986b
James Farr <jamesfarr1@sky.com>
2025-05-18 19:16:29 +0100

    feat(survey page): styling and snimation added

--- src/pages/SurveyPage.tsx
+++ src/pages/SurveyPage.tsx
@@ -2,7 +2,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import { fetchSurvey, submitSurvey } from "../lib/api";
 import type { Survey, SurveyResponseItem } from "../lib/interface";
-import { CgMenuGridO } from "react-icons/cg";
+import { motion } from "framer-motion";
 import toast from "react-hot-toast";
 import LoadingSpinner from "../components/LoadingSpinner";
 import ErrorMessage from "../components/ErrorMessage";
@@ -100,17 +100,21 @@ const SurveyPage = () => {
     return (
         <div>
             <Header />
-            <div>
+            <div className="flex items-baseline bg-gray-100 w-full min-h-screen p-6 justify-center">
                 {isLoading && <LoadingSpinner />}
 
                 {error && (
                     <ErrorMessage error={error} />
                 )}
                 {survey && (
-                    <div>
-                        <div>
-                            <p>{survey.title}</p>
-                            <p>{survey.description}</p>
+                    <motion.div  
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.3, delay: 0.3 }} 
+                    className="md:py-5 md:px-6 p-3 border-gray-300 border-[1px] rounded-2xl bg-white shadow-md w-full md:w-[60%]">
+                        <div className="mx-4 shadow-[0_2px_3px_-2px_rgba(0,0,0,0.1)] pb-3">
+                            <p className="font-bold text-center mb-1">{survey.title}</p>
+                            <p className="text-sm md:text-base text-center">{survey.description}</p>
                         </div>
                         <div>
                             <form onSubmit={(e) => {
@@ -118,52 +122,55 @@ const SurveyPage = () => {
                                 handleSubmit();
                             }}>
                                 {survey.questions.map((item) => (
-                                    <div key={item.id}>
-                                        <label>{item.question}</label>
+                                    <div className="mx-4 shadow-[0_2px_3px_-2px_rgba(0,0,0,0.1)] pb-3 pt-3" key={item.id}>
+                                        <label className="block w-full text-left md:text-center text-sm md:text-base">{item.question}</label>
                                         {item.type === "multiple_choice" || item.type === "single_choice" && validationErrors[item.id] && (
-                                            <p className="text-red-500">
+                                            <p className="text-red-500 font-semibold md:text-center">
                                                 {validationErrors[item.id]}
                                             </p>
                                         )}
-                                        {/* single choice */}
-                                        {item.type === "single_choice" && item.options?.map((option) => (
-                                            <div key={option}>
-                                                <input
-                                                    type="radio"
-                                                    name={item.id}
-                                                    value={option}
-                                                    onChange={() => handleChange(item.id, option)}
-                                                />
-                                                {option}
-                                            </div>
-                                        ))}
-                                        {/* multiple choice */}
-                                        {item.type === "multiple_choice" && item.options?.map((option) => (
-                                            <div key={option}>
-                                                <input
-                                                    type="checkbox"
-                                                    name={`${item.id}-${option}`}
-                                                    value={option}
-                                                    onChange={(e) => {
-                                                        const selected = responses.find((response) => response.question_id === item.id)?.selected_option.split(",") || [];
-                                                        const updated = e.target.checked ? [...selected, option]
-                                                            : selected.filter((opt) => opt !== option);
-                                                        handleChange(item.id, updated)
-                                                    }}
-                                                />
-                                                {option}
-                                            </div>
-                                        ))}
+                                        <div className="flex md:justify-center gap-4 md:items-center flex-col md:flex-row mt-3 mx-auto">
+                                            {/* single choice */}
+                                            {item.type === "single_choice" && item.options?.map((option) => (
+                                                <div className="flex gap-1 text-sm mt-1" key={option}>
+                                                    <input
+                                                        className="text-sm"
+                                                        type="radio"
+                                                        name={item.id}
+                                                        value={option}
+                                                        onChange={() => handleChange(item.id, option)}
+                                                    />
+                                                    {option}
+                                                </div>
+                                            ))}
+                                            {/* multiple choice */}
+                                            {item.type === "multiple_choice" && item.options?.map((option) => (
+                                                <div className="flex gap-1 text-sm mt-1" key={option}>
+                                                    <input
+                                                        type="checkbox"
+                                                        name={`${item.id}-${option}`}
+                                                        value={option}
+                                                        onChange={(e) => {
+                                                            const selected = responses.find((response) => response.question_id === item.id)?.selected_option.split(",") || [];
+                                                            const updated = e.target.checked ? [...selected, option]
+                                                                : selected.filter((opt) => opt !== option);
+                                                            handleChange(item.id, updated)
+                                                        }}
+                                                    />
+                                                    {option}
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
                                 ))}
-                                <div>
-                                    <button type="submit">
+                                <div className="mt-2 py-2 flex justify-center">
+                                    <button className="hover:bg-emerald-300 transition-colors duration-300 cursor-pointer bg-emerald-400 py-2 rounded-full px-5 text-sm " type="submit">
                                         Submit Survey
                                     </button>
                                 </div>
                             </form>
                         </div>
-                    </div>
+                    </motion.div>
                 )}
             </div>
         </div>
